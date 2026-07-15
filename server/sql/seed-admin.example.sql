8e5c74277d3c0b74c0eb482eadfd674603f1a8f6
markovalexey36-del <markovalexey36@gmail.com>
2026-07-16 00:29:19 +0300

    Update seed-admin.example.sql

--- server/sql/seed-admin.example.sql
+++ server/sql/seed-admin.example.sql
@@ -4,7 +4,7 @@ INSERT INTO admins (id, login, password_hash, is_active)
 VALUES (
   '11111111-1111-1111-1111-111111111111',
   'admin',
-  '$2a$12$REPLACE_WITH_BCRYPT_HASH',
+  '$2a$12$W3ryTcXMiTPbiuDzB9XxWeMO6021kbwvB./TTmBFElUV4jP6ybBFi',
   1
 )
 ON DUPLICATE KEY UPDATE
